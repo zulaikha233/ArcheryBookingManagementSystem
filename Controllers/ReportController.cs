7b2e8ab57ebef8510a5c29bbcb713cd893c9914b
MUHAMMAD AZAD BIN MASRUN <TP076178@mail.apu.edu.my>
2026-06-05 10:06:21 +0800

    Mock Performance Report Page. Non Functional.

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcheryAlley.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IArcheryAlleyRepository _repository;
+
+        public ReportController(IArcheryAlleyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult StudentPerformance()
+        {
+            var role = HttpContext.Session.GetString("UserRole");
+            if (string.IsNullOrEmpty(role))
+                return RedirectToAction("Login", "Account");
+
+            ViewBag.StaffName = HttpContext.Session.GetString("UserName");
+            return View("~/Views/Staff/StudentPerformanceReport.cshtml");
+        }
+    }
+}
