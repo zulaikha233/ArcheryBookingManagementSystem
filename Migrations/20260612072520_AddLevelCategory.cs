c72f2d10b84dd992c5555dabe14a5785100d3625
Zulaikha <zulaikhalatif2@gmail.com>
2026-06-12 17:53:24 +0800

    standardize ui guest mode

---
+++ Migrations/20260612072520_AddLevelCategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArcheryAlley.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevelCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LevelCategory",
+                table: "Archers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LevelCategory",
+                table: "Archers");
+        }
+    }
+}
